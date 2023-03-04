bea5ba0797c9f1256661e30276bea76a3184d37b
Charlie.com <csru22005@g.nihon-u.ac.jp>
2023-03-04 15:09:38 +0000

    Air_Quality_Sensor_Sample

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
-#include "mbed.h"
+#include"mbed.h"
+#include"Air_Quality.h"
+AirQuality airqualitysensor;
+int current_quality = -1;
+PinName analogPin = p15;
 
-DigitalOut myled(LED1);
+// Interrupt Handler
+void AirQualityInterrupt()
+{
+    AnalogIn sensor(analogPin);
+    airqualitysensor.last_vol = airqualitysensor.first_vol;
+    airqualitysensor.first_vol = sensor.read()*1000;
+    airqualitysensor.timer_index = 1;
+}
 
-int main() {
-  while(1) {
-    myled = 1;
-    wait(0.2);
-    myled = 0;
-    wait(0.2);
-  }
+// Main loop
+int main()
+{
+    airqualitysensor.init(analogPin, AirQualityInterrupt);
+    while(1) {
+        current_quality=airqualitysensor.slope();
+        if (current_quality >= 0) { // if a valid data returned.
+            if (current_quality == 0)
+                printf("High pollution! Force signal active\n\r");
+            else if (current_quality == 1)
+                printf("High pollution!\n\r");
+            else if (current_quality == 2)
+                printf("Low pollution!\n\r");
+            else if (current_quality == 3)
+                printf("Fresh air\n\r");
+        }
+    }
 }
